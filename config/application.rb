39b06338d3e5303174e43f707abce540b824afc9
Guilherme <guilhermefeitosa66@gmail.com>
2015-12-13 00:48:55 -0300

    set scaffold template

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,12 @@ Bundler.require(*Rails.groups)
 
 module Classificador
   class Application < Rails::Application
+
+    # disable scaffold stylesheet generator
+    config.generators do |g|
+      g.stylesheets false
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -18,7 +24,7 @@ module Classificador
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :'pt-BR'
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
